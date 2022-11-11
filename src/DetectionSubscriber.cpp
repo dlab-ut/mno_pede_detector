eeebcec1251c79140d7c80f9c25b7de5700626fc
meno-k <meno-k@roboken.iit.tsukuba.ac.jp>
2022-11-11 18:00:48 +0900

    way point処理追加

--- src/DetectionSubscriber.cpp
+++ src/DetectionSubscriber.cpp
@@ -5,12 +5,14 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "std_msgs/msg/header.hpp"
 #include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/int16.hpp"
 #include "image_transport/image_transport.hpp"
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <vector>
 
 #include "darknet_ros_msgs/msg/mno.hpp"
 #include "darknet_ros_msgs/msg/bounding_boxes.hpp"
@@ -41,99 +43,148 @@ int redCount = 0;
 int blueCount = 0;
 int lightState = 0; // 1:赤、2：赤の後の青、0：その他
 bool existTrafficLight = false;
+bool isRecognize = false;
+std::vector<int> recognizeWayPoints;
 
 class DetectionSubscriber : public rclcpp::Node
 {
 public:
   DetectionSubscriber()
-  : Node("detection_subscriber")
+      : Node("detection_subscriber")
   {
-    subscription_ = this->create_subscription<darknet_ros_msgs::msg::Mno>(
-      std::string("/mno_topic"), 10, std::bind(&DetectionSubscriber::mno_callback, this, _1));
+    subscription_mno = this->create_subscription<darknet_ros_msgs::msg::Mno>(
+        std::string("/mno_topic"), 10, std::bind(&DetectionSubscriber::mnoCallback, this, _1));
+    subscription_way = this->create_subscription<std_msgs::msg::Int16>(
+        std::string("/way_point"), 10, std::bind(&DetectionSubscriber::wayPointCallback, this, _1));
     publisher_ = this->create_publisher<std_msgs::msg::Bool>("is_runnable", 10);
+    recognizeWayPoints.push_back(10);
+    recognizeWayPoints.push_back(20);
   }
 
 private:
-  void mno_callback(const darknet_ros_msgs::msg::Mno::SharedPtr msg) const
+  void mnoCallback(const darknet_ros_msgs::msg::Mno::SharedPtr msg) const
   {
     cv::Mat camImageMat, clipImage, hsvImage, maskImageRed, maskImageBlue;
     double whiteAreaRatio;
+    if (isRecognize)
+    {
+      if (!existTrafficLight)
+      {
+        RCLCPP_INFO(get_logger(), "check for traffic light.");
+        imageCount++;
+        if (msg->is_detected)
+          trafficLightCount++;
+        if (imageCount >= CHECK_COUNT)
+          existTrafficLight = filter_temporal();
+      }
+      else
+      {
+        if (msg->is_detected)
+        {
+          // RCLCPP_INFO(get_logger(), "check red or blue");
+          trafficLightCount++;
 
-    if(!existTrafficLight){
-      RCLCPP_INFO(get_logger(), "[mno_pede_detector] check for traffic light.");
-      imageCount++;
-      if(msg->is_detected) trafficLightCount++;
-      if(imageCount >= CHECK_COUNT) existTrafficLight = filter_temporal();
-    }else{
-      if(msg->is_detected){
-        // RCLCPP_INFO(get_logger(), "[mno_pede_detector] check red or blue");
-        trafficLightCount++;
+          // 画像を取得して切り抜き
+          cv_bridge::CvImagePtr cam_image = cv_bridge::toCvCopy(msg->image, "bgr8");
+          camImageMat = cam_image->image.clone();
+          int xmin, xmax, ymin, ymax;
+          xmin = msg->bounding_boxes.bounding_boxes[0].xmin;
+          xmax = msg->bounding_boxes.bounding_boxes[0].xmax;
+          ymin = msg->bounding_boxes.bounding_boxes[0].ymin;
+          ymax = msg->bounding_boxes.bounding_boxes[0].ymax;
+          clipImage = cv::Mat(camImageMat, cv::Rect(xmin, ymin, xmax - xmin, ymax - ymin)).clone();
+          // cv::namedWindow("clipImage");
+          // cv::imshow("clipImage", clipImage);
+          // cv::waitKey(10);
 
-        // 画像を取得して切り抜き
-        cv_bridge::CvImagePtr cam_image = cv_bridge::toCvCopy(msg->image, "bgr8");
-        camImageMat = cam_image->image.clone();
-        int xmin, xmax, ymin, ymax;
-        xmin = msg->bounding_boxes.bounding_boxes[0].xmin;
-        xmax = msg->bounding_boxes.bounding_boxes[0].xmax;
-        ymin = msg->bounding_boxes.bounding_boxes[0].ymin;
-        ymax = msg->bounding_boxes.bounding_boxes[0].ymax;
-        clipImage = cv::Mat(camImageMat, cv::Rect(xmin, ymin, xmax-xmin, ymax-ymin)).clone();
-        // cv::namedWindow("clipImage");
-        // cv::imshow("clipImage", clipImage);
-        // cv::waitKey(10);
+          // hsv画像に変換
+          cv::cvtColor(clipImage, hsvImage, CV_BGR2HSV, 3);
 
-        // hsv画像に変換
-        cv::cvtColor(clipImage, hsvImage, CV_BGR2HSV, 3);
+          // 赤のフィルター
+          maskImageRed = filter_color(hsvImage, RED_H_MAX, RED_H_MIN, RED_S_MAX, RED_S_MIN, RED_V_MAX, RED_V_MIN);
+          cv::namedWindow("maskImageRed");
+          cv::imshow("maskImageRed", maskImageRed);
+          cv::waitKey(1);
 
-        // 赤のフィルター
-        maskImageRed = filter_color(hsvImage, RED_H_MAX, RED_H_MIN, RED_S_MAX, RED_S_MIN, RED_V_MAX, RED_V_MIN);
-        cv::namedWindow("maskImageRed");
-        cv::imshow("maskImageRed", maskImageRed);
-        cv::waitKey(1);
+          whiteAreaRatio = calculate_white_ratio(maskImageRed);
+          RCLCPP_INFO(get_logger(), "whiteAreaRatio red = %f", whiteAreaRatio);
+          if (whiteAreaRatio > THRESHOLD_RATIO)
+          {
+            RCLCPP_INFO(get_logger(), "red light");
+            redCount++;
+          }
 
-        whiteAreaRatio = calculate_white_ratio(maskImageRed);
-        std::cout << whiteAreaRatio << std::endl;
-        if(whiteAreaRatio > THRESHOLD_RATIO){
-          RCLCPP_INFO(get_logger(), "[mno_pede_detector] red light");
-          redCount++;
+          // 青のフィルター
+          maskImageBlue = filter_color(hsvImage, BLUE_H_MAX, BLUE_H_MIN, BLUE_S_MAX, BLUE_S_MIN, BLUE_V_MAX, BLUE_V_MIN);
+          cv::namedWindow("maskImageBlue");
+          cv::imshow("maskImageBlue", maskImageBlue);
+          cv::waitKey(1);
+          whiteAreaRatio = calculate_white_ratio(maskImageBlue);
+          RCLCPP_INFO(get_logger(), "whiteAreaRatio blue = %f", whiteAreaRatio);
+          if (whiteAreaRatio > THRESHOLD_RATIO)
+          {
+            RCLCPP_INFO(get_logger(), "blue light");
+            blueCount++;
+          }
+
+          if (trafficLightCount >= CHECK_COUNT)
+          {
+            determine_red_blue();
+          }
         }
 
-        // 青のフィルター
-        maskImageBlue = filter_color(hsvImage, BLUE_H_MAX, BLUE_H_MIN, BLUE_S_MAX, BLUE_S_MIN, BLUE_V_MAX, BLUE_V_MIN);
-        cv::namedWindow("maskImageBlue");
-        cv::imshow("maskImageBlue", maskImageBlue);
-        cv::waitKey(1);
-        whiteAreaRatio = calculate_white_ratio(maskImageBlue);
-        if(whiteAreaRatio > THRESHOLD_RATIO){
-          RCLCPP_INFO(get_logger(), "[mno_pede_detector] blue light");
-          blueCount++;
+        if (lightState == 2)
+        {
+          std_msgs::msg::Bool msg = std_msgs::msg::Bool();
+          msg.data = true;
+          publisher_->publish(msg);
+          RCLCPP_INFO(this->get_logger(), "publish true");
+          imageCount = 0;
+          trafficLightCount = 0;
+          redCount = 0;
+          blueCount = 0;
+          lightState = 0;
+          existTrafficLight = false;
+          isRecognize = false;
         }
-
-        if(trafficLightCount >= CHECK_COUNT) {
-          determine_red_blue();
+        else
+        {
+          std_msgs::msg::Bool msg = std_msgs::msg::Bool();
+          msg.data = false;
+          publisher_->publish(msg);
+          RCLCPP_INFO(this->get_logger(), "publish false");
         }
       }
+    }
+  }
 
-      if(lightState == 2){
-        std_msgs::msg::Bool msg = std_msgs::msg::Bool();
-        msg.data = true;
-        publisher_->publish(msg);
-        RCLCPP_INFO(this->get_logger(), "[mno_pede_detector] publish true");
-      }else{
-        std_msgs::msg::Bool msg = std_msgs::msg::Bool();
-        msg.data = false;
-        publisher_->publish(msg);
-        RCLCPP_INFO(this->get_logger(), "[mno_pede_detector] publish false");
+  void wayPointCallback(const std_msgs::msg::Int16::SharedPtr msg) const
+  {
+    int wayPoint = msg->data;
+    int deleteIndex = -1;
+    for (size_t i = 0; i < recognizeWayPoints.size(); i++)
+    {
+      if (wayPoint == recognizeWayPoints[i])
+      {
+        deleteIndex = i;
+        isRecognize = true;
       }
+    }
+    if (deleteIndex != -1)
+    {
+      recognizeWayPoints.erase(recognizeWayPoints.begin() + deleteIndex);
     }
   }
 
   bool filter_temporal() const
   {
-    if((double)trafficLightCount/(double)imageCount > 0.7) {
+    if ((double)trafficLightCount / (double)imageCount > 0.7)
+    {
       trafficLightCount = 0;
       return true;
-    }else{
+    }
+    else
+    {
       imageCount = 0;
       trafficLightCount = 0;
       return false;
@@ -151,20 +202,25 @@ private:
 
   double calculate_white_ratio(cv::Mat src) const
   {
-    int whole_area = src.size().width*src.size().height;
+    int whole_area = src.size().width * src.size().height;
     int white_area = cv::countNonZero(src);
-    return (double)white_area/(double)whole_area;
+    return (double)white_area / (double)whole_area;
   }
 
   void determine_red_blue() const
   {
-    double redRatio = (double)redCount/(double)trafficLightCount;
-    if(redRatio > DECISION_RATE) {
+    double redRatio = (double)redCount / (double)trafficLightCount;
+    if (redRatio > DECISION_RATE)
+    {
       lightState = 1;
-    }else{
-      double blueRatio = (double)blueCount/(double)trafficLightCount;
-      if(blueRatio > DECISION_RATE){
-        if(lightState == 1) lightState = 2;
+    }
+    else
+    {
+      double blueRatio = (double)blueCount / (double)trafficLightCount;
+      if (blueRatio > DECISION_RATE)
+      {
+        if (lightState == 1)
+          lightState = 2;
       }
     }
     redCount = 0;
@@ -172,12 +228,12 @@ private:
     trafficLightCount = 0;
   }
 
-  rclcpp::Subscription<darknet_ros_msgs::msg::Mno>::SharedPtr subscription_;
-  rclcpp::Subscription<darknet_ros_msgs::msg::BoundingBoxes>::SharedPtr boundingBoxSubscription;
+  rclcpp::Subscription<darknet_ros_msgs::msg::Mno>::SharedPtr subscription_mno;
+  rclcpp::Subscription<std_msgs::msg::Int16>::SharedPtr subscription_way;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr publisher_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<DetectionSubscriber>());
